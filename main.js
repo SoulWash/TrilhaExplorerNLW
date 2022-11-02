18e71471c5d36c260cd1cc430e9641878b70fc70
Washington Luiz De Sousa <washluiz_@hotmail.com>
2022-11-02 12:50:06 -0300

    correção dia

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ document.querySelector("#app").innerHTML = `
     </header>
     <main id="cards">
         ${createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameroon") + createGame("uruguay", "10:00", "southkorea") + createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia"))}
-        ${createCard("28/11", "quinta", createGame("cameroon", "07:00", "serbia") + createGame("southkorea", "10:00", "ghana") + createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguay"))}
-        ${createCard("02/12", "quinta", createGame("southkorea", "12:00", "portugal") + createGame("ghana", "12:00", "uruguay") + createGame("serbia", "16:00", "switzerland") + createGame("cameroon", "16:00", "brazil"))}
+        ${createCard("28/11", "segunda", createGame("cameroon", "07:00", "serbia") + createGame("southkorea", "10:00", "ghana") + createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00", "uruguay"))}
+        ${createCard("02/12", "sexta", createGame("southkorea", "12:00", "portugal") + createGame("ghana", "12:00", "uruguay") + createGame("serbia", "16:00", "switzerland") + createGame("cameroon", "16:00", "brazil"))}
     </main>
 `
